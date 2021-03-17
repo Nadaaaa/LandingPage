683f3ecccbdaf5184b8602270192373747ab8225
Nadaaaa <nadabadr95@gmail.com>
2021-03-17 18:15:49 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -12,39 +12,28 @@
  * JS Standard: ESlint
  *
  */
-/**
- * Define Global Variables
- *
- */
-let arr = [
+
+/* Define Global Variables */
+const SectionsStringArray = [
     "section1",
     "section2",
     "section3",
     "section4"
 ];
+/* End Global Variables*/
 
 
 // build the nav
-let unorderedList = document.getElementById('navbar__list');
-for (let i = 0; i < arr.length; i++) {
-    let listItem = document.createElement('li');
-    listItem.innerText = arr[i];
-    let id_string = 'nav_sec' + ((i + 1).toString());
-    listItem.id = id_string
-    unorderedList.append(listItem)
-    document.getElementById(id_string).classList.add('menu__link')
-}
-document.getElementById('nav_sec1').classList.add('active');
-
+buildNavigation();
 
 const sectionsLinks = document.querySelectorAll(".menu__link");
 const sections = document.querySelectorAll('section');
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
+
+
+
+/* Start Helper Functions */
+
 function removeAllActiveClasses() {
     sectionsLinks.forEach(item => {
         item.classList.remove('active');
@@ -54,65 +43,77 @@ function removeAllActiveClasses() {
 function addActiveClass(id) {
     let element
     if (id === 'section1') {
-        element = document.getElementById('nav_sec1')
+        element = document.getElementById('nav_sec1');
     }
 
     if (id === 'section2') {
-        element = document.getElementById('nav_sec2')
+        element = document.getElementById('nav_sec2');
     }
 
     if (id === 'section3') {
-        element = document.getElementById('nav_sec3')
+        element = document.getElementById('nav_sec3');
     }
 
     if (id === 'section4') {
-        element = document.getElementById('nav_sec4')
+        element = document.getElementById('nav_sec4');
     }
-    element.classList.add("active")
+    element.classList.add("active");
 }
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
+/* End Helper Functions */
 
 
+
+/* Begin Main Functions */
+
+// Build the navigation
+function buildNavigation(){
+    let unorderedList = document.getElementById('navbar__list');
+    for (let i = 0; i < SectionsStringArray.length; i++) {
+        let listItem = document.createElement('li');
+        listItem.innerText = SectionsStringArray[i];
+        let id_string = 'nav_sec' + ((i + 1).toString());
+        listItem.id = id_string;
+        unorderedList.append(listItem);
+        document.getElementById(id_string).classList.add('menu__link');
+    }
+    document.getElementById('nav_sec1').classList.add('active');
+}
 
 // Add class 'active' to section when near top of viewport
+function setActiveSection(section){
+    let sectionPosition = document.getElementById(section.id).getBoundingClientRect();
 
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu 
-
-// Scroll to section on link click
-
-onscroll = function () {
-    let scrollPosition = document.documentElement.scrollTop;
-
-    sections.forEach(item => {
-        if (scrollPosition >= item.offsetTop - item.offsetHeight * 0.5 && scrollPosition <= item.offsetTop + item.offsetHeight * 0.5) {
-            let currentId = item.id
-            removeAllActiveClasses();
-            addActiveClass(currentId);
-        }
-    })
+    if (sectionPosition.top < 300) {
+        let currentId = section.id;
+        removeAllActiveClasses();
+        addActiveClass(currentId);
+    }
 }
 
+// Scroll to anchor ID using scrollTO event
+function scrollToSection(section){
+    document.getElementById(section).scrollIntoView({behavior: "smooth"});
+}
+/* End Main Functions */
 
-// Set sections as active
 
-sectionsLinks.forEach((item) => {
-    item.addEventListener("click", () => {
-        let section = document.getElementById(item.innerHTML)
-        section.scrollIntoView({behavior: "smooth", block: "end"})
+/* Begin Events */
+
+// Scroll to section on link click
+sectionsLinks.forEach((sectionLink) => {
+    sectionLink.addEventListener("click", () => {
+        scrollToSection(sectionLink.innerHTML);
     })
 })
+
+// Set sections as active
+onscroll = function () {
+    sections.forEach(section => {
+        setActiveSection(section)
+    })
+}
+/* End Events */
+
+
+
+
